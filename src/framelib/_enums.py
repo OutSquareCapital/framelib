a718c4f225690a0bff6de43bb5d69e386bd1fb59
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-09-10 23:37:58 +0200

    reorganized methods order, added a few convenience methodes for series, list and Iterables interactions

--- src/framelib/_enums.py
+++ src/framelib/_enums.py
@@ -1,58 +1,14 @@
+from collections.abc import Iterable
 from enum import StrEnum
 from typing import Literal, Self
 
 import polars as pl
+import pychain as pc
 
 Kind = Literal["name", "value"]
 
 
 class Enum(StrEnum):
-    @classmethod
-    def to_series(cls, kind: Kind = "value") -> pl.Series:
-        """Convert the Enum members to a Polars Series.
-
-        Example:
-            >>> class MyEnum(Enum):
-            ...     value1 = "value1"
-            ...     value2 = "value2"
-            ...     value3 = "value3"
-            >>> MyEnum.to_series().to_list()
-            ['value1', 'value2', 'value3']
-        """
-        values: list[str] = cls.to_list(kind)
-        return pl.Series(cls.__name__, values, dtype=pl.Enum(values))
-
-    @classmethod
-    def to_list(cls, kind: Kind = "value") -> list[str]:
-        """Return the Enum members as a plain Python list.
-
-        Example:
-            >>> class MyEnum(Enum):
-            ...     value1 = "value1"
-            ...     value2 = "value2"
-            ...     value3 = "value3"
-            >>> MyEnum.to_list()
-            ['value1', 'value2', 'value3']
-        """
-        match kind:
-            case "value":
-                return [member.value for member in cls]
-            case "name":
-                return [member.name for member in cls]
-
-    @classmethod
-    def to_dtype(cls, kind: Kind = "value") -> pl.Enum:
-        """Return a Polars Enum dtype for this Enum.
-
-        Example:
-            >>> class MyEnum(Enum):
-            ...     a = "a"
-            ...     b = "b"
-            >>> MyEnum.to_dtype()
-            Enum(categories=['a', 'b'])
-        """
-        return pl.Enum(cls.to_list(kind))
-
     @classmethod
     def from_df(cls, data: pl.DataFrame | pl.LazyFrame, name: str) -> Self:
         """Create a dynamic Enum from values present in a DataFrame column.
@@ -73,11 +29,76 @@ class Enum(StrEnum):
         )
 
     @classmethod
-    def from_series(cls, data: pl.Series) -> Self:
+    def from_series(cls, data: pl.Series):
         """Create a dynamic Enum from a Series.
 
         Example:
-            >>> Enum.from_series(pl.Series(["value3", "value1", "value2", "value1"])).to_list()
-            ['value1', 'value2', 'value3']
+            >>> Enum.from_series(pl.Series(["a", "a", "b", "c"])).to_list()
+            ['a', 'b', 'c']
         """
         return cls(data.name, data.unique().sort().to_list())
+
+    @classmethod
+    def from_iter(cls, data: Iterable[str], name: str) -> Self:
+        """Create a dynamic Enum from an iterable of strings.
+
+        Example:
+            >>> Enum.from_iter(data=["a", 'a', "b", "c"], name="foo").to_list()
+            ['a', 'b', 'c']
+        """
+
+        return cls(name, sorted(pc.Iter(data).unique().into_list().unwrap()))
+
+    @classmethod
+    def to_series(cls, kind: Kind = "value") -> pl.Series:
+        """Convert the Enum members to a Polars Series.
+
+        Example:
+            >>> class MyEnum(Enum):
+            ...     value1 = "value1"
+            ...     value2 = "value2"
+            ...     value3 = "value3"
+            >>> MyEnum.to_series().to_list()
+            ['value1', 'value2', 'value3']
+        """
+        values = cls.to_list(kind).unwrap()
+        return pl.Series(cls.__name__, values, dtype=pl.Enum(values))
+
+    @classmethod
+    def to_iter(cls, kind: Kind = "value") -> pc.Iter[str]:
+        """Return the Enum members as a plain Python list.
+
+        Example:
+            >>> class MyEnum(Enum):
+            ...     value1 = "value1"
+            ...     value2 = "value2"
+            ...     value3 = "value3"
+            >>> MyEnum.to_list()
+            ['value1', 'value2', 'value3']
+        """
+        match kind:
+            case "value":
+                return pc.Iter(member.value for member in cls)
+            case "name":
+                return pc.Iter(member.name for member in cls)
+
+    @classmethod
+    def to_list(cls, kind: Kind = "value") -> pc.List[str]:
+        """Return the Enum members as a pychain List.
+
+        Syntactic sugar for `to_iter().into_list()`.
+        """
+        return cls.to_iter(kind).into_list()
+
+    @classmethod
+    def to_dtype(cls, kind: Kind = "value") -> pl.Enum:
+        """Return a Polars Enum dtype for this Enum.
+
+        Example:
+            >>> class MyEnum(Enum):
+            ...     a = "a"
+            ...     b = "b"
+            >>> MyEnum.to_dtype()
+            Enum(categories=['a', 'b'])
+        """
+        return pl.Enum(cls.to_iter(kind).unwrap())
