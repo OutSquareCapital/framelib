bd495f0dae7a2493e3daf5cfb0a7251473a58f66
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-09-30 23:59:08 +0200

    refactor tree module by deleting the class and simplifying it as a pure function, and deleting the CSS part

--- src/framelib/_tree.py
+++ src/framelib/_tree.py
@@ -1,9 +1,14 @@
-from dataclasses import dataclass
-from enum import StrEnum, auto
-from html import escape
+from enum import StrEnum
 from pathlib import Path
 
 import pychain as pc
+
+
+def show_tree(path: Path) -> str:
+    """
+    Returns a string representation of the directory tree starting from the given path.
+    """
+    return f"{path}\n" + "\n".join(_build_tree(path))
 
 
 class Tree(StrEnum):
@@ -13,30 +18,12 @@ class Tree(StrEnum):
     SPACE = "    "
 
 
-class CSS(StrEnum):
-    DIR = auto()
-    FILE = auto()
-    STYLE = """
-        <style>
-            .tree ul { list-style-type: none; padding-left: 20px; }
-            .tree li { position: relative; padding-left: 25px; line-height: 1.8; }
-            .tree li::before, .tree li::after {
-                content: ''; position: absolute; left: 0;
-            }
-            .tree li::before {
-                border-left: 1px solid #999; height: 100%; top: 0; width: 0;
-            }
-            .tree li:last-child::before { height: 20px; }
-            .tree li::after {
-                border-top: 1px solid #999; height: 0; top: 20px; width: 18px;
-            }
-        </style>
-        """
-
-
 def _build_tree(directory: Path, prefix: str = "") -> list[str]:
     lines: list[str] = []
-    items: pc.Iter[Path] = _paths(directory)
+    items: pc.Iter[Path] = pc.Iter(directory.iterdir()).sort(
+        key=lambda p: (p.is_file(), p.name.lower())
+    )
+
     items_length: int = items.length()
 
     for i, item in items.enumerate().unwrap():
@@ -47,51 +34,9 @@ def _build_tree(directory: Path, prefix: str = "") -> list[str]:
     return lines
 
 
-def _build_html_tree(directory: Path) -> str:
-    html = "<ul>"
-    _paths(directory).map(lambda path: _map_paths(html, path))
-    html += "</ul>"
-    return html
-
-
-def _map_paths(html: str, path: Path) -> None:
-    html += f'<li class="{_class(path)}">{escape(path.name)}'
-    if path.is_dir():
-        html += _build_html_tree(path)
-    html += "</li>"
-
-
-def _paths(directory: Path) -> pc.Iter[Path]:
-    return pc.Iter(directory.iterdir()).sort(
-        key=lambda p: (p.is_file(), p.name.lower())
-    )
-
-
-def _class(item: Path) -> CSS:
-    return CSS.DIR if item.is_dir() else CSS.FILE
-
-
 def _connector(is_last: bool) -> Tree:
     return Tree.LAST_NODE if is_last else Tree.NODE
 
 
 def _continuation(is_last: bool) -> Tree:
     return Tree.SPACE if is_last else Tree.BRANCH
-
-
-@dataclass(slots=True, repr=False)
-class TreeDisplay:
-    root: Path
-
-    def __repr__(self) -> str:
-        return f"{self.root}\n" + "\n".join(_build_tree(self.root))
-
-    def _display_(self) -> str:
-        return f'<div class="tree">{CSS.STYLE}{self._html_header}{_build_html_tree(self.root)}</div>'
-
-    def _repr_html_(self) -> str:
-        return self._display_()
-
-    @property
-    def _html_header(self) -> str:
-        return f"'<code>{self.root}</code>'"
