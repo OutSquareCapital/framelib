7568860c72451aa3ea6b0c1ecca89691eb01643b
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2026-01-18 13:22:55 +0100

    cleaning up tests, going from scratch

--- tests/_data.py
+++ tests/_data.py
@@ -37,6 +37,7 @@ class DataFrames:
                 "charlie@example.com",
             ],
         },
+        schema=Customers.to_pl(),
     )
 
     SALES = pl.DataFrame(
@@ -45,6 +46,7 @@ class DataFrames:
             "customer_id": [101, 102, 103],
             "amount": [10.0, 20.0, 30.0],
         },
+        schema=Sales.to_pl(),
     )
     CONFLICTING_SALES = pl.DataFrame(
         {"order_id": [2, 4], "customer_id": [102, 104], "amount": [99.9, 40.0]},
